f2f11b42ae9728ee73824729fa6e0fa8b615da4b
fjmiguel <fjmiguel@106-16.info2>
2022-10-20 14:40:47 +0200

    Ejercicios-Tema2

--- Ejercicios-Tema2/src/ejercicio4/Ejercicio4.java
+++ Ejercicios-Tema2/src/ejercicio4/Ejercicio4.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
-		int num;
+		int num, unidadesNum, decenasNum = 0;
+		String unidades, decenas, num0;
+		
 
 		Scanner read = new Scanner (System.in);
 		
@@ -13,9 +15,90 @@ public class Ejercicio4 {
 		
 		num = read.nextInt();
 		
-
-	
-
+		decenasNum=num/10;
+		unidadesNum=num%10;
+		
+		if (num<0||num>99) {
+			System.out.println("El número introducido no está comprendido ente 1 y 99");
+		}
+		
 	}
 
 }
+		
+//		else {
+//			switch (num){
+//			case 1:
+//				unidades="uno";
+//			case 2:
+//				unidades="dos";
+//			case 3:
+//				unidades="tres";
+//			case 4:
+//				unidades="cuatro";	
+//			case 5:
+//				unidades="cinco";
+//			case 6:
+//				unidades="seis";
+//			case 7:
+//				unidades="siete";
+//			case 8:
+//				unidades="ocho";;
+//			case 9:
+//				unidades="nueve";
+//			}
+//				if (unidadesNum==0) {
+//					switch (num) {
+//					case 20:
+//						num0="veinte";
+//					case 30:
+//						num0="treinta";
+//					case 40:
+//						num0="cuarenta";
+//					case 50:
+//						num0="cincuenta";
+//					case 60:
+//						num0="sesenta";
+//					case 70:
+//						num0="setenta";
+//					case 80:
+//						num0="ochenta";
+//					case 90:
+//						num0="noventa";
+//					}
+//				}
+//				else if (num>=21&&num<=29) {
+//						decenas="veinti";
+//							System.out.println(decenas+unidades);
+//						
+//			}
+//			
+//			
+//				}
+//			}
+//			
+//		}
+			
+	
+
+		
+	
+
+	
+
+
+
+//1,3,4,6
+
+//case 10:
+//	System.out.println("diez");
+//case 11:
+//	System.out.println("once");
+//case 12:
+//	System.out.println("doce");
+//case 13:
+//	System.out.println("trece");
+//case 14:
+//	System.out.println("catorce");
+//case 15:
+//	System.out.println("quince");
--- Ejercicios-Tema2/src/ejercicio6/Ejercicio6.java
+++ Ejercicios-Tema2/src/ejercicio6/Ejercicio6.java
@@ -5,7 +5,27 @@ import java.util.Scanner;
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
-
+		int numAleatorio1, numAleatorio2, suma, resultado;
+		
+		numAleatorio1= (int)(Math.random() * 99 +1);
+		numAleatorio2= (int)(Math.random() * 99 +1);
+		suma= numAleatorio1 + numAleatorio2;
+		
+		System.out.print(numAleatorio1 + " + " + numAleatorio2 + " = ");
+		
+		Scanner read = new Scanner (System.in);
+		resultado = read.nextInt();
+		
+		if(resultado==suma) {
+			System.out.println("¡BIEN! El resultado de la suma es correcto");
+		}
+		
+		else {
+			System.out.println("UPS... El resultado de la suma es erróneo");
+		}
+		
+		read.close();
+		
 	}
 
 }
