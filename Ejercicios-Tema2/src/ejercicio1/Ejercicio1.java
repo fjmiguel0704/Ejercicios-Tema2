7ee3f10496ba93f12a736f9f7ddb576016c7daf1
Fernando <Fernando@DESKTOP-3EE8PP0>
2022-10-24 23:46:20 +0200

    Ejercicios-Tema2

--- Ejercicios-Tema2/src/ejercicio1/Ejercicio1.java
+++ Ejercicios-Tema2/src/ejercicio1/Ejercicio1.java
@@ -3,50 +3,85 @@ package ejercicio1;
 import java.util.Scanner;
 
 public class Ejercicio1 {
-
+	//Pruebas:
+	//-Introducir 5=capicúa.
+	//-Introducir 55=capicúa.
+	//Introducir 656=capicúa.
+	//-Introducir 4114=capicúa.
+	//-Introducir 25=no capicúa.
+	//-Introducir 955=no capicúa.
+	//-Introducir 1515=no capicúa.
 	public static void main(String[] args) {
-		//Creamos la variable num  
-		int num, invertido = 0,  aux, cifra;
+		//Creamos la variables tipo int. 
+		int num, unidades, decenas, centenas, millares, numInvert = 0;
+		int decInvert, cenInvert, millInvert;
+
 		
 		//Creamos el Scanner para leer por el teclado todo lo introducido por el usuario
 		Scanner read = new Scanner (System.in);
 		
 		//Le pedimos al usuario que ingrese un número. Comprendido entre 0 y 9999
 		System.out.println("Introduce un número comprendido en un rango del 0 al 9999: ");
-		
+
 		//Declaramos la variable num, que guardará el número introducido por el usuario
-		num = read.nextInt();
+		num = read.nextInt();                                        
+
+		//Declaramos la variable unidades. Esta guardará el resto de la división de la variable num entre 10
+		unidades = num % 10;
 		
-		//Declaro la variable unidades. Recogerá el resto de la división de la variable num entre 10
-		//unidades= num%10;
-		
-		//Declaro la variable decenas. 
-	//	decenas= (num/10) %10;
-		//centenas= (num/100) %10;
-		//millares= (num/1000);
-		
-		if (num<0 || num>9999) {
-			System.out.println("ERROR: El número introducido no está comprendido en el rango que se solicita");
-		} 
-		
-		while (num < 10);
-	
-        aux = num;
+		//Declaramos la variable decenas. Esta guardará el resto de la división de la variable num entre 100
+        decenas = num % 100;
         
-        while (aux!=0){
-            cifra = aux % 10;
-            invertido = invertido * 10 + cifra;
-            aux = aux / 10;
-        }
-    
-        if(num == invertido){
-            System.out.println("Es capicua");
-        }else{
-            System.out.println("No es capicua");
+      //Declaramos la variable centenas. Esta guardará el resto de la división de la variable num entre 1000
+        centenas = num % 1000;
+
+        //Declaramos la variable decInvert. Guardará el resultado de dividir la variable decenas entre 10 y nos devolverá las decenas invertidas
+        decInvert = decenas / 10;
+        
+        //Declaramos la variable cenInvert. Guardará el resultado de dividir la variable decenas entre 100 y nos devolverá las centenas invertidas
+        cenInvert = centenas / 100;
+        
+        //Declaramos la variable millInvert. Guardará el resultado de dividir la variable decenas entre 1000 y nos devolverá los millares invertidos
+        millInvert = num / 1000;
+
+        //si num es menor que 0 y mayor a 99999, que muestre un error
+        if (num < 0 || num > 99999) {
+            System.out.print("ERROR: el número introducido no es correcto");
+        } 
+
+        //En caso contrario, si num es mayor o igual a 0 y menor o igual a 9, numInvert será igual a num
+        else if (num >= 0 && num <= 9) { 
+            numInvert = num;
         } 
         
-        	//Cerramos el scanner
-      		read.close();
+        //En caso contrario, si numero es mayor a 9 y menor a 100, numero invertido será el resultado de multiplicar las unidades por 10 y sumarle las decenas invertidas
+        else if (num > 9 && num < 100){ 
+        	numInvert = (unidades * 10) + (decInvert);
+        } 
+        
+      //En caso contrario, si numero es mayor a 99 y menor a 1000, numero invertido será el resultado de multiplicar las unidades por 100 y sumarle las decenas invertidas por 10 + las centenas invertidas
+        else if (num > 99 && num < 1000){ 
+        	numInvert = (unidades * 100) + (decInvert * 10) + (cenInvert);
+        } 
+        
+      //En caso contrario, si numero es mayor a 999 y menor a 10000, numero invertido será el resultado de multiplicar las unidades por 10 y sumarle las decenas invertidas por 100 mas las centenas invertidas por 10 mas los millares invertidos
+        else if (num > 999 && num < 10000){ 
+        	numInvert = (unidades * 1000) + (decInvert * 100) + (cenInvert * 10) + (millInvert);
+        }
+        
+        //Si lo anterior se cumple, el número es capicúa
+        if (num == numInvert) {
+                System.out.println("Es capicúa.");
+        }
+        
+        //Si no, no lo es
+        else {
+                System.out.println("No es capicúa.");
+        }
+        
+        //Cerramos el Scanner
+        read.close();
+
 		}
 	}
 
--- Ejercicios-Tema2/src/ejercicio4/Ejercicio4.java
+++ Ejercicios-Tema2/src/ejercicio4/Ejercicio4.java
@@ -3,102 +3,190 @@ package ejercicio4;
 import java.util.Scanner;
 
 public class Ejercicio4 {
-
+	//Pruebas: 
+	//-Introducimos 8.
+	//-Introducimos 20.
+	//-Introducimoa 14.
+	//-Introducimos 16.
+	//-Introducimos 74.
 	public static void main(String[] args) {
-		int num, unidadesNum, decenasNum = 0;
-		String unidades, decenas, num0;
 		
-
+		//Creamos las variables. num recogerá el númerpo introducido por el usuario y decenas y unidades el primer y segundo número de él
+		int num, unidades, decenas;
+		
+		//Creamos otras dos variables tipo string. Guardarán el primer ny segundo número en formato texto
+		String primeraLetra="", segundaLetra="";
+		
+		//Creamos el Scanner para leer por el teclado todo lo introducido por el usuario
 		Scanner read = new Scanner (System.in);
 		
+		//Le pedimos al usuario que introduzca un número entre el 1 y el 99
 		System.out.println("Introduce un número entero que se encuentre '1-99': ");
 		
+		//Declaramos la variable num, que recogerá el número introducido por el usuario
 		num = read.nextInt();
 		
-		decenasNum=num/10;
-		unidadesNum=num%10;
+		//Declaramos la variable unidades. Esta guardará el resto de la división de la variable num entre 10
+		unidades=num%10;
 		
-		if (num<0||num>99) {
+		//Declaramo la variable decenas, que guardará el resultado de la división de la variable num entre 10
+		decenas=num/10;
+		
+		//Indicamos, que si el número ntroducido es mayor o igual a 0 o mayor a 99 muestre un error
+		if (num<=0||num>99) {
 			System.out.println("El número introducido no está comprendido ente 1 y 99");
 		}
+		
+		//Las unidades
+        switch (unidades) {
+        
+        //En caso de ser 0
+        case 0:
+        	
+        	//La primera letra recogerá "cero"
+            segundaLetra = "cero";
+            
+            //Frenamos la cadena
+            break;
+            
+            //Así sucesivamente hasta nueve
+        case 1:
+        	segundaLetra = "uno";
+            break;
+        case 2:
+        	segundaLetra = "dos";
+            break;
+        case 3:
+        	segundaLetra = "tres";
+            break;
+        case 4:
+        	segundaLetra = "cuatro";
+            break;
+        case 5:
+        	segundaLetra = "cinco";
+            break;
+        case 6:
+        	segundaLetra = "seis";
+            break;
+        case 7:
+        	segundaLetra = "siete";
+            break;
+        case 8:
+        	segundaLetra = "ocho";
+            break;
+        case 9:
+        	segundaLetra = "nueve";
+            break;
+    }
+        
+        //Las decenas
+        switch (decenas) {
+        
+        //En caso de ser 2
+		case 2: 
+			
+			//La primerá letra recogerá "veinte"
+			primeraLetra="veinte";
+			
+			//Frenamos la cadena
+			break;
+			
+			//Así hasta 90
+		case 3: 
+			primeraLetra="treinta";
+			break;
+		case 4: 
+			primeraLetra="cuarenta";
+			break;
+		case 5: 
+			primeraLetra="cincuenta";
+			break;
+		case 6: 
+			primeraLetra="sesenta";
+			break;
+		case 7: 
+			primeraLetra="setenta";
+			break;
+		case 8: 
+			primeraLetra="ochenta";
+			break;
+		case 9: 
+			primeraLetra="noventa";
+			break;
+        }
+        
+        
+        //Si numero es mayor o igual a 1 y menor o igual a 9 que muestre únicamente la segunda letra del número
+        if(num>=1&&num<=9) {
+        	System.out.println(segundaLetra);
+        }
+        
+        //En caso contrario, si num es mayor o igual a 10 y menor o igual a 15
+        else if(num>=10&&num<=15){
+        	
+        	//Las unidades
+        	switch (unidades) {
+        	
+        	//En caso de ser 0
+			case 0: 
+				
+				//La segunda letra del número recogerá diez
+				segundaLetra="diez";
+				
+				//Frenamos la cadena
+				break;
+				
+				//Así hasta 15
+			case 1: 
+				segundaLetra="once";
+				break;
+			case 2: 
+				segundaLetra="doce";
+				break;
+			case 3: 
+				segundaLetra="trece";
+				break;
+			case 4: 
+				segundaLetra="catorce";
+				break;
+			case 5: 
+				segundaLetra="quince";
+				break;
+			}
+        	
+        	//Y mostramos la segunda letra del número
+        	System.out.println(segundaLetra);
+        }
+        
+        //En caso contrario, si num es mayor o igual a 16 y menor o igual a 19
+        else if(num>=16&&num<=19) {
+        	
+        	//La primera letra será dieci y se mostrará la primera letra mas la segunda
+        	primeraLetra="dieci";
+        	System.out.println(primeraLetra+segundaLetra);
+        }
+        
+      //En caso contrario, si unidades es igual a 0 y decenas distinto 0, muestra la primera letra solamente
+        else if(unidades==0&&decenas!=0) {
+        	System.out.println(primeraLetra);
+        }
+        
+        //En caso contrario, si numero es mayor o igual a 21 y menor o igual a 29, la primera letra recogerá "veinti", y a ella sumale la segunda
+        else if(num>=21&&num<=29) {
+        	primeraLetra="veinti";
+        	System.out.println(primeraLetra+segundaLetra);
+        }
+        
+        //En caso contrario, si numero es mayor o igual a 21 y menor o igual a 99, a la primera letra sumale la segunda
+        else if(num>=31&&num<=99) {
+        	System.out.println(primeraLetra+ " y "+segundaLetra);
+        }
+        
+        //Cerramos el Scanner
+        read.close();
+		
 		
 	}
 
 }
-		
-//		else {
-//			switch (num){
-//			case 1:
-//				unidades="uno";
-//			case 2:
-//				unidades="dos";
-//			case 3:
-//				unidades="tres";
-//			case 4:
-//				unidades="cuatro";	
-//			case 5:
-//				unidades="cinco";
-//			case 6:
-//				unidades="seis";
-//			case 7:
-//				unidades="siete";
-//			case 8:
-//				unidades="ocho";;
-//			case 9:
-//				unidades="nueve";
-//			}
-//				if (unidadesNum==0) {
-//					switch (num) {
-//					case 20:
-//						num0="veinte";
-//					case 30:
-//						num0="treinta";
-//					case 40:
-//						num0="cuarenta";
-//					case 50:
-//						num0="cincuenta";
-//					case 60:
-//						num0="sesenta";
-//					case 70:
-//						num0="setenta";
-//					case 80:
-//						num0="ochenta";
-//					case 90:
-//						num0="noventa";
-//					}
-//				}
-//				else if (num>=21&&num<=29) {
-//						decenas="veinti";
-//							System.out.println(decenas+unidades);
-//						
-//			}
-//			
-//			
-//				}
-//			}
-//			
-//		}
-			
-	
 
-		
-	
-
-	
-
-
-
-//1,3,4,6
-
-//case 10:
-//	System.out.println("diez");
-//case 11:
-//	System.out.println("once");
-//case 12:
-//	System.out.println("doce");
-//case 13:
-//	System.out.println("trece");
-//case 14:
-//	System.out.println("catorce");
-//case 15:
-//	System.out.println("quince");
